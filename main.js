e9fcf53ef92e2a48258dd6603e42ec878950aac0
Byron Perez <xxloboisthemanxx@gmail.com>
2021-09-27 21:27:33 -0300

    clase2

---
+++ main.js
@@ -0,0 +1,77 @@
+/*let catFat = Tito;
+
+if(catFat == Tito){
+    console.log("Sapee");
+}
+
+
+let userName = prompt ("Ingrese Usuario");
+
+if(userName == "Jorge"){
+
+    console.log("Bienvenido Jorge");
+
+}
+else if(userName == "El bananero"){
+    console.log("Sapeee");
+}
+else{
+    console.log("Quien te conoce bro");
+}
+
+
+
+let userName = prompt ("Ingrese Nombre");
+let userAge = prompt ("Ingrese Edad");
+
+if (userAge >= "18"){
+
+    console.log("Puedes votar");
+
+}
+else if (userAge < "18"){
+
+    console.log("Flaco recatate");
+
+}
+
+
+
+let numberX = prompt ("Ingrese Numero");
+
+if (numberX % "2" == "0"){
+
+console.log ("Es un num. par");
+
+}
+else{
+
+    console.log ("NUm. inpar");
+}
+
+*/
+
+let numberOne = prompt("Ingrese num");
+let numberTwo = prompt("Ingrese num");
+let numberThree = prompt("Ingrese num");
+
+numberOne = parseFloat(numberOne);
+numberTwo = parseFloat(numberTwo);
+numberThree = parseFloat(numberThree);
+
+
+
+if (numberOne > numberTwo && numberOne > numberThree){
+
+console.log("el primer numero es el mayor");
+}
+
+else if (numberTwo > numberOne && numberTwo > numberThree){
+
+console.log("el segundo numero es el mayor");
+}
+
+else {
+    console.log("el tercer numero es el mayor");
+}
+
